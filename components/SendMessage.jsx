17b414d68a43978d3b99a06e238ab5c3c9e78148
Kimminguk-kor <65965280+Kimminguk-kor@users.noreply.github.com>
2020-12-22 22:18:23 +0900

    before_pull_commit

--- components/SendMessage.jsx
+++ components/SendMessage.jsx
@@ -1,18 +1,18 @@
 
 export default function SendMessage() {
     return (
-        <div class="md:flex bg-yellow-200 h-flex w-full">
-        <div class="relative inline-block text-left ">
-          <div>
-            <select class="inline-flex justify-center w-full rounded-md border border-gray-300
-            shadow-sm px-7 py-4 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50
-            ">      
-            <option>POST</option>
-            <option>GET</option>
-            </select>
-          </div>
-          <textarea></textarea>
-          <button class="border border-gray-400 bg-white">실행</button>
+        <div class="md:flex bg-yellow-200 h-flex w-full flex flex-wrap">
+          <div class="relative inline-block text-left">
+            <div>
+              <select class="inline-flex justify-center w-1/6 rounded-md border border-gray-300
+              shadow-sm px-7 py-4 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50
+              ">      
+              <option>POST</option>
+              <option>GET</option>
+              </select>
+            <textarea class="line-flex w-36"></textarea>
+            </div>
+          <button class="w-12 border border-gray-400 bg-white">실행</button>
         </div>
       </div>
     )
