8634da321617ec660ed0022134fe3743f41ffde0
이준희 <tate@ijunhuis-MacBook-Pro.local>
2021-02-20 09:06:10 +0900

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 const colors = require("tailwindcss/colors");
 
+// module.exports = {
+//   purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {
+//       colors: {
+//         orange: colors.orange,
+//       },
+//     },
+//   },
+// };
+
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
@@ -10,4 +21,21 @@ module.exports = {
       },
     },
   },
-};
+  plugins: [
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+      
+    ],
+  ],
+}
